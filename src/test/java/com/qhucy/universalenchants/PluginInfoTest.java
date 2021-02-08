1c192b6141d5bc67859677910530e5d81d33dfbb
Qhucy <qhucy@protonmail.com>
2021-02-08 01:46:04 -0800

    Added unit testing for PluginInfo.

---
+++ src/test/java/com/qhucy/universalenchants/PluginInfoTest.java
@@ -0,0 +1,64 @@
+package com.qhucy.universalenchants;
+
+import org.bukkit.plugin.InvalidDescriptionException;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.junit.jupiter.api.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("PluginInfo Class Testing")
+final class PluginInfoTest
+{
+
+    private static PluginDescriptionFile dummyPluginDescriptionFile;
+    private PluginInfo pluginInfo;
+
+    @BeforeAll
+    static void setUpAll()
+    {
+        try
+        {
+            dummyPluginDescriptionFile = new PluginDescriptionFile(new FileInputStream("src/test/java/com/qhucy" +
+                    "/universalenchants/dummy_plugin.yml"));
+        }
+        catch (InvalidDescriptionException | FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @BeforeEach
+    void setUp()
+    {
+        pluginInfo = new PluginInfo(dummyPluginDescriptionFile);
+    }
+
+    @Test
+    @DisplayName("Getting Plugin Name")
+    void getName()
+    {
+        assertEquals(pluginInfo.getName(), "name");
+    }
+
+    @Test
+    @DisplayName("Getting Plugin Version")
+    void getVersion()
+    {
+        assertEquals(pluginInfo.getVersion(), "version");
+    }
+
+    @Test
+    @DisplayName("Getting Plugin Authors")
+    void getAuthors()
+    {
+        final String[] authors = pluginInfo.getAuthors();
+
+        assertEquals(authors.length, 1);
+        assertEquals(authors[0], "author");
+    }
+
+}
