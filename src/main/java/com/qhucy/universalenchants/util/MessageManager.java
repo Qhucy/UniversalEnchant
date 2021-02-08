efaa595279b230619286e27481eaa5f020f126dd
Qhucy <qhucy@protonmail.com>
2021-02-08 03:32:53 -0800

    Added utility class for modifying messages including colorization and replacing variables.

---
+++ src/main/java/com/qhucy/universalenchants/util/MessageManager.java
@@ -0,0 +1,90 @@
+package com.qhucy.universalenchants.util;
+
+import lombok.NonNull;
+import org.bukkit.ChatColor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that contains static utility methods for modifying and sending messages.
+ *
+ * @author Qhucy
+ * @since 08 Feb, 2021
+ */
+public final class MessageManager
+{
+
+    // List of color codes supported by Spigot.
+    private final static List<Character> COLOR_CODES;
+
+    // Loads the color codes character array from the ChatColor enumeration constants.
+    static
+    {
+        final ArrayList<Character> chatColorCharacters = new ArrayList<>();
+
+        for (final ChatColor chatColor : ChatColor.values())
+        {
+            chatColorCharacters.add(chatColor.getChar());
+        }
+
+        COLOR_CODES = chatColorCharacters;
+    }
+
+    /**
+     * Returns the given message with color code variables replaced by color code values.
+     *
+     * @param message The message. Can't be null.
+     *
+     * @return The given message with color code variables replaced by color code values.
+     */
+    public static String colorize(@NonNull final String message)
+    {
+        final StringBuilder stringBuilder = new StringBuilder();
+        final char[] chars = message.toCharArray();
+
+        for (int i = 0; i < chars.length; i++)
+        {
+            char part = chars[i];
+
+            if (part == '&' && (i + 1) < chars.length && COLOR_CODES.contains(chars[i + 1]))
+            {
+                stringBuilder.append(ChatColor.COLOR_CHAR).append(chars[i + 1]);
+
+                i++;
+            }
+            else
+            {
+                stringBuilder.append(part);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Replaces variables inside of a message with their values.
+     *
+     * @param message              The message that contains variables. Can't be null.
+     * @param replacementVariables The variables in the message to replace with their values. Length must be a
+     *                             multiple of two with the format [variable, value, variable, value, ...]. Can be null.
+     *
+     * @return The stringValue with its variables being replaced with its values.
+     */
+    public static String replaceVariables(@NonNull String message, final String[] replacementVariables)
+    {
+        if (replacementVariables != null && replacementVariables.length > 1 && replacementVariables.length % 2 == 0)
+        {
+            for (int i = 0; i < replacementVariables.length; i += 2)
+            {
+                final String variable = replacementVariables[i];
+                final String value = replacementVariables[i + 1];
+
+                message = message.replace(variable, value);
+            }
+        }
+
+        return message;
+    }
+
+}
---
+++ src/test/java/com/qhucy/universalenchants/util/MessageManagerTest.java
@@ -0,0 +1,104 @@
+package com.qhucy.universalenchants.util;
+
+import org.bukkit.ChatColor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static com.qhucy.universalenchants.util.MessageManager.*;
+
+@DisplayName("MessageManager Testing")
+class MessageManagerTest
+{
+
+    final static char colorChar = ChatColor.COLOR_CHAR;
+
+    @Test
+    @DisplayName("Colorizing Empty Message")
+    void colorizeEmptyMessage()
+    {
+        assertEquals(colorize(""), "");
+    }
+
+    @Test
+    @DisplayName("Colorizing Without Colors")
+    void colorizeWithNoColors()
+    {
+        final String base = "This is a message.";
+
+        assertEquals(colorize(base), base);
+    }
+
+    @Test
+    @DisplayName("Colorizing With One Color")
+    void colorizeWithOneColor()
+    {
+        assertEquals(colorize("&cMessage"), colorChar + "cMessage");
+    }
+
+    @Test
+    @DisplayName("Colorizing With Multiple Colors")
+    void colorizeWithMultipleColors()
+    {
+        assertEquals(colorize("&aHello &bWorld&e!"), colorChar + "aHello " + colorChar + "bWorld" + colorChar + "e!");
+    }
+
+    @Test
+    @DisplayName("Replacing Message With Null Variables")
+    void replaceVariablesWithNullVariables()
+    {
+        final String base = "This is a message.";
+
+        assertEquals(replaceVariables(base, null), base);
+    }
+
+    @Test
+    @DisplayName("Replacing Message With No Variables")
+    void replaceVariablesWithNoVariables()
+    {
+        final String base = "This is a message.";
+
+        assertEquals(replaceVariables(base, new String[]{}), base);
+    }
+
+    @Test
+    @DisplayName("Replacing Message With Invalid Variables Length")
+    void replaceVariablesWithInvalidVariablesLength()
+    {
+        final String base = "This is a message.";
+
+        assertEquals(replaceVariables(base, new String[]{"length must be", "divisible", "by 2"}), base);
+    }
+
+    @Test
+    @DisplayName("Replacing Message With One Variable")
+    void replaceVariablesWithOneVariable()
+    {
+        assertEquals(replaceVariables("I am from {state}.", new String[]{"{state}", "California"}), "I am from " +
+                "California.");
+    }
+
+    @Test
+    @DisplayName("Replacing Message With One Variable And Multiple Occurrences")
+    void replaceVariablesWithOneVariableAndMultipleOccurrences()
+    {
+        assertEquals(replaceVariables("I am from {state} and my mother is from {state}.", new String[]{"{state}",
+                "California"}), "I am from California and my mother is from California.");
+    }
+
+    @Test
+    @DisplayName("Replacing Message With Variables And No Occurrence")
+    void replaceVariablesWithVariablesAndNoOccurrence()
+    {
+        assertEquals(replaceVariables("Hello world!", new String[]{"{money}", "0"}), "Hello world!");
+    }
+
+    @Test
+    @DisplayName("Replacing Message With Multiple Variables and Occurrences")
+    void replaceVariablesWithMultipleVariablesAndOccurrences()
+    {
+        assertEquals(replaceVariables("!{car}:{element}#{element}*{car}({color}", new String[]{"{car}", "Tesla",
+                "{element}", "Fire", "{color}", "purple"}), "!Tesla:Fire#Fire*Tesla(purple");
+    }
+
+}
