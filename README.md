5e2e922b062eac4d6446d65ef101b3fd6f7be038
Qhucy <qhucy@protonmail.com>
2021-02-08 00:37:12 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# UniversalEnchants
+Minecraft plugin for SpigotMC 1.16.5 that adds a custom currency,
+a plethora of custom enchantments, and a way to add and modify it all to your needs.
---
+++ src/main/java/com/qhucy/universalenchants/UniversalEnchants.java
@@ -0,0 +1,15 @@
+package com.qhucy.universalenchants;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main class for UniversalEnchants (UE) that handles the flow of enabling and disabling the plugin.
+ *
+ * @author Qhucy
+ */
+public class UniversalEnchants extends JavaPlugin
+{
+
+
+
+}
