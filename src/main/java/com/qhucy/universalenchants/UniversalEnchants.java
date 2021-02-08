4a3ece3d1490f905fff57a09f7d9281647224e4a
Qhucy <qhucy@protonmail.com>
2021-02-08 04:39:33 -0800

    Added basic enabling and disabling methods.

--- src/main/java/com/qhucy/universalenchants/UniversalEnchants.java
+++ src/main/java/com/qhucy/universalenchants/UniversalEnchants.java
@@ -3,13 +3,29 @@ package com.qhucy.universalenchants;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Main class for UniversalEnchants (UE) that handles the flow of enabling and disabling the plugin.
+ * Main class for UniversalEnchants (UE) that initializes the foundation of the plugin.
  *
  * @author Qhucy
+ * @since 08 Feb, 2021
  */
-public class UniversalEnchants extends JavaPlugin
+public final class UniversalEnchants extends JavaPlugin
 {
 
+    public static final boolean DEBUG = true;
 
+    // Data for basic information about this plugin.
+    private PluginInfo pluginInfo;
+
+    @Override
+    public final void onEnable()
+    {
+        pluginInfo = new PluginInfo(getDescription());
+    }
+
+    @Override
+    public final void onDisable()
+    {
+
+    }
 
 }
